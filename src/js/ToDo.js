f40934d1003501b0a5f8695edb8bbf1a93198aa6
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-11-11 00:32:56 +0200

    adds functionality to the ToDo app. Changes to be committed: 	modified:   src/js/ToDo.js 	modified:   src/js/items.json 	modified:   src/pages/module-06-11.html 	deleted:    src/partials/example.html 	modified:   src/sass/_module-06-11.scss

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -5,10 +5,11 @@ import items from './items';
 const refs = {
   body: document.body,
   form: document.querySelector('.add-task-form'),
-  list: document.querySelector('.task-list'),
-  input: document.querySelector('.add-task-form__input'),
   addBtn: document.querySelector('.add-task-form__btn'),
+  list: document.querySelector('.task-list'),
+  taskText: document.querySelector('.task-list__text'),
 };
+
 //---------------------------- function getItemTamplate ---------------------------------------------
 /**
  * Function returns string (tamplate for markup) of task list
@@ -23,28 +24,12 @@ const getItemTamplate = items => {
             <input class="task-list__check" type="checkbox" ${isDone ? 'checked' : ''}/>
             <span class="task-list__text">${task}</span>
           </div>
-          <button class="task-list__delbtn" type="button"><img src="./src/images/iconmonstr-trash-can-10.svg" alt="X" width="24"/></button>
+           <button class="task-list__delbtn" type="button"></button>
         </li>`,
     )
     .join('');
 };
 
-//---------------------------- function addNewTask  ------------------------------------------------
-/**
- * Function adds new task to task list
- * @param {Event} event
- * @returns
- */
-const addNewTask = event => {
-  event.preventDefault();
-  const newTask = {
-    task: event.target.elements.text.value,
-    isDone: false,
-  };
-  items.push(newTask);
-  refs.form.reset();
-  render();
-};
 //------------------------------ function render  ------------------------------------------------
 /**
  * Function renders list of tasks in DOM
@@ -56,8 +41,50 @@ const render = () => {
   refs.list.insertAdjacentHTML('beforeend', getItemTamplate(items));
 };
 
+//---------------------------- function addTaskToTaskArray  ------------------------------------------------
+function addTaskToTaskArray(event) {
+  const newTask = {
+    task: event.target.elements.text.value,
+    isDone: false,
+  };
+  items.push(newTask);
+}
+
+//---------------------------- function deleteTaskFromTaskArray  ------------------------------------------------
+function deleteTaskFromTaskArray(event) {
+  // finde target object in array
+  const curentTaskText = event.target.parentNode.querySelector('.task-list__text').textContent;
+  const curentObjIndex = items.findIndex(item => item.task === curentTaskText);
+  // delet object from array
+  items.splice(curentObjIndex, 1);
+}
+
+//---------------------------- function onAddTaskBtnClick  ------------------------------------------------
+/**
+ * Function adds new task to task list
+ * @param {Event} event
+ * @returns
+ */
+const onAddTaskBtnClick = event => {
+  event.preventDefault();
+  addTaskToTaskArray(event);
+  refs.form.reset();
+  render();
+};
+
+//---------------------------- function onDeleteBtnClick  ------------------------------------------------
+const onDeleteBtnClick = event => {
+  if (!event.target.classList.contains('task-list__delbtn')) {
+    return;
+  }
+  deleteTaskFromTaskArray(event); // delete element from data-model array
+  event.target.parentNode.remove(); // delete element from DOM
+};
+
 //----------------------- add event listner on form submit, and adds new task  ---------------------
-refs.form.addEventListener('submit', addNewTask);
+refs.form.addEventListener('submit', onAddTaskBtnClick);
+refs.list.addEventListener('click', onDeleteBtnClick);
 
 // run
 render();
+console.table(items);
